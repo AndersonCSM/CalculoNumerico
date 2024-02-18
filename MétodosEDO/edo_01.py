f11eda9868ded2eb804058bd92c992299ce003c6
AndersonCSM <96142983+AndersonCSM@users.noreply.github.com>
2024-02-18 15:52:47 -0300

    +

--- MétodosEDO/edo_01.py
+++ MétodosEDO/edo_01.py
@@ -2,12 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from euler import *
 
+# EDO de PVI da página 288 do livro Análise numérica de Burden 10 edição
+# Author: Anderson Carlos da Silva Morais | https://github.com/AndersonCSM
+
 
 # Definindo a EDO: dy/dx = x + y
 def edo(y, t):
     return y - t**2 + 1
 
 
+# Solução exata
 def exata(t):
     return (t + 1)**2 - 0.5 * np.exp(t)
 
@@ -22,8 +26,8 @@ h = 0.2  # Tamanho do passo
 # Aplicando o método de Euler
 x_p, y_p = euler_method(edo, y0, t0, n, h)
 
-t_points = [t0 + i*h for i in range(n+1)]
-y_exato = [exata(t) for t in t_points]
+x_points = [t0 + i * h for i in range(n + 1)]
+y_exato = [exata(t) for t in x_points]
 
 plt.plot(x_p, y_p, label='Método de Euler')
 plt.plot(x_p, y_exato, label='Valores exatos')
